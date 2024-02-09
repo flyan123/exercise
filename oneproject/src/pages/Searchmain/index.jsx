2c94af1141cd8908407471d87c913b4e0f4f0b33
flyan123 <2495365901@qq.com>
2024-02-09 23:27:59 +0800

    add 小修改

--- oneproject/src/pages/Searchmain/index.jsx
+++ oneproject/src/pages/Searchmain/index.jsx
@@ -23,6 +23,10 @@ function FilterableProductTable({ products }) {
         <SearchBar
           filterText={filterText}
           onFilterTextChange={setFilterText} />
+          <br/>
+          <br/>
+          <br/>
+          <br/>
         <ProductTable
           products={products}
           filterText={filterText} />
@@ -56,5 +60,343 @@ function SearchBar({
         </div>
     )
 }
+// 内容框
+function ProductTable({ products, filterText }) {
+    const rows = [];
+    products.forEach((product) => {
+      if (
+        product.name.toLowerCase().indexOf(
+          filterText.toLowerCase()
+        ) === -1
+      ) {
+        return;
+      }
+      
+      rows.push(
+        <ProductRow
+          product={product}
+          key={product.name} />
+      );
+    });
+  
+    return (
+      <table>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+
+  function ProductRow({ product }) {
+
+    return (
+      <tr>
+          <div className='tu-kuang'>
+          <ul >
+                    <li>
+                        <div className='seletu-name' >
+                            {/* 图片 */}
+                            <div className='seletu-tu'>
+                                <a href='#' >
+                                    <div >
+                                        {/* src={src} */}
+                                        <img
+                                            src="https://p0.pipi.cn/mmdb/fb7386515370fa07ac87a9b97732f64f519a1.jpg?imageView2/1/w/218/h/300" />
+                                    </div>
+                                </a>
+                            </div>
+                            {/* 购票小标 */}
+                            <div className='seletu-biao'>购票</div>
+                            {/* 电影名：name={name} */}
+                            <div className='seletu-tu-name'><a href='#' style={{ color: 'black' }}> 临时劫案</a></div>
+                            {/* 评分：score={score} */}
+                            <div className='seletu-tu-score'>暂无评分</div>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div className='seletu-name' >
+                            {/* 图片 */}
+                            <div className='seletu-tu'>
+                                <a href='#' >
+                                    <div >
+                                        {/* src={src} */}
+                                        <img
+                                            src="https://p0.pipi.cn/mmdb/fb73862fb53c7ec696b5355e4ffa645077ef6.jpg?imageView2/1/w/218/h/300" />
+                                    </div>
+                                </a>
+                            </div>
+                            {/* 购票小标 */}
+                            <div className='seletu-biao'>购票</div>
+                            {/* 电影名：name={name} */}
+                            <div className='seletu-tu-name'><a href='#' style={{ color: 'black' }}> 天降大任</a></div>
+                            {/* 评分：score={score} */}
+                            <div className='seletu-tu-score'>暂无评分</div>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div className='seletu-name' >
+                            {/* 图片 */}
+                            <div className='seletu-tu'>
+                                <a href='#' >
+                                    <div >
+                                        {/* src={src} */}
+                                        <img
+                                            src="https://p0.pipi.cn/mmdb/fb7386929ab51b9235c696581b610612ce1a9.jpg?imageView2/1/w/218/h/300" />
+                                    </div>
+                                </a>
+                            </div>
+                            {/* 购票小标 */}
+                            <div className='seletu-biao'>购票</div>
+                            {/* 电影名：name={name} */}
+                            <div className='seletu-tu-name'><a href='#' style={{ color: 'black' }}> 大雨</a></div>
+                            {/* 评分：score={score} */}
+                            <div className='seletu-tu-score'>暂无评分</div>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div className='seletu-name' >
+                            {/* 图片 */}
+                            <div className='seletu-tu'>
+                                <a href='#' >
+                                    <div >
+                                        {/* src={src} */}
+                                        <img
+                                            src="https://p0.meituan.net/mmc/f5523879ec43bdcbda484c5f0e7cf3262776.png@170w_118h_1e_1c" />
+                                    </div>
+                                </a>
+                            </div>
+                            {/* 购票小标 */}
+                            <div className='seletu-biao'>购票</div>
+                            {/* 电影名：name={name} */}
+                            <div className='seletu-tu-name'><a href='#' style={{ color: 'black' }}> ...</a></div>
+                            {/* 评分：score={score} */}
+                            <div className='seletu-tu-score'>暂无评分</div>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div className='seletu-name' >
+                            {/* 图片 */}
+                            <div className='seletu-tu'>
+                                <a href='#' >
+                                    <div >
+                                        {/* src={src} */}
+                                        <img
+                                            src="https://p0.meituan.net/mmc/f5523879ec43bdcbda484c5f0e7cf3262776.png@170w_118h_1e_1c" />
+                                    </div>
+                                </a>
+                            </div>
+                            {/* 购票小标 */}
+                            <div className='seletu-biao'>购票</div>
+                            {/* 电影名：name={name} */}
+                            <div className='seletu-tu-name'><a href='#' style={{ color: 'black' }}> ...</a></div>
+                            {/* 评分：score={score} */}
+                            <div className='seletu-tu-score'>暂无评分</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className='seletu-name' >
+                            {/* 图片 */}
+                            <div className='seletu-tu'>
+                                <a href='#' >
+                                    <div >
+                                        {/* src={src} */}
+                                        <img
+                                            src="https://p0.meituan.net/mmc/f5523879ec43bdcbda484c5f0e7cf3262776.png@170w_118h_1e_1c" />
+                                    </div>
+                                </a>
+                            </div>
+                            {/* 购票小标 */}
+                            <div className='seletu-biao'>购票</div>
+                            {/* 电影名：name={name} */}
+                            <div className='seletu-tu-name'><a href='#' style={{ color: 'black' }}> ...</a></div>
+                            {/* 评分：score={score} */}
+                            <div className='seletu-tu-score'>暂无评分</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className='seletu-name' >
+                            {/* 图片 */}
+                            <div className='seletu-tu'>
+                                <a href='#' >
+                                    <div >
+                                        {/* src={src} */}
+                                        <img
+                                            src="" />
+                                    </div>
+                                </a>
+                            </div>
+                            {/* 购票小标 */}
+                            <div className='seletu-biao'>购票</div>
+                            {/* 电影名：name={name} */}
+                            <div className='seletu-tu-name'><a href='#' style={{ color: 'black' }}> ...</a></div>
+                            {/* 评分：score={score} */}
+                            <div className='seletu-tu-score'>暂无评分</div>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div className='seletu-name' >
+                            {/* 图片 */}
+                            <div className='seletu-tu'>
+                                <a href='#' >
+                                    <div >
+                                        {/* src={src} */}
+                                        <img
+                                            src="" />
+                                    </div>
+                                </a>
+                            </div>
+                            {/* 购票小标 */}
+                            <div className='seletu-biao'>购票</div>
+                            {/* 电影名：name={name} */}
+                            <div className='seletu-tu-name'><a href='#' style={{ color: 'black' }}> ...</a></div>
+                            {/* 评分：score={score} */}
+                            <div className='seletu-tu-score'>暂无评分</div>
+                        </div>
+                    </li><li>
+                        <div className='seletu-name' >
+                            {/* 图片 */}
+                            <div className='seletu-tu'>
+                                <a href='#' >
+                                    <div >
+                                        {/* src={src} */}
+                                        <img
+                                            src="" />
+                                    </div>
+                                </a>
+                            </div>
+                            {/* 购票小标 */}
+                            <div className='seletu-biao'>购票</div>
+                            {/* 电影名：name={name} */}
+                            <div className='seletu-tu-name'><a href='#' style={{ color: 'black' }}> ...</a></div>
+                            {/* 评分：score={score} */}
+                            <div className='seletu-tu-score'>暂无评分</div>
+                        </div>
+                    </li><li>
+                        <div className='seletu-name' >
+                            {/* 图片 */}
+                            <div className='seletu-tu'>
+                                <a href='#' >
+                                    <div >
+                                        {/* src={src} */}
+                                        <img
+                                            src="" />
+                                    </div>
+                                </a>
+                            </div>
+                            {/* 购票小标 */}
+                            <div className='seletu-biao'>购票</div>
+                            {/* 电影名：name={name} */}
+                            <div className='seletu-tu-name'><a href='#' style={{ color: 'black' }}> ...</a></div>
+                            {/* 评分：score={score} */}
+                            <div className='seletu-tu-score'>暂无评分</div>
+                        </div>
+                    </li><li>
+                        <div className='seletu-name' >
+                            {/* 图片 */}
+                            <div className='seletu-tu'>
+                                <a href='#' >
+                                    <div >
+                                        {/* src={src} */}
+                                        <img
+                                            src="" />
+                                    </div>
+                                </a>
+                            </div>
+                            {/* 购票小标 */}
+                            <div className='seletu-biao'>购票</div>
+                            {/* 电影名：name={name} */}
+                            <div className='seletu-tu-name'><a href='#' style={{ color: 'black' }}> ...</a></div>
+                            {/* 评分：score={score} */}
+                            <div className='seletu-tu-score'>暂无评分</div>
+                        </div>
+                    </li><li>
+                        <div className='seletu-name' >
+                            {/* 图片 */}
+                            <div className='seletu-tu'>
+                                <a href='#' >
+                                    <div >
+                                        {/* src={src} */}
+                                        <img
+                                            src="" />
+                                    </div>
+                                </a>
+                            </div>
+                            {/* 购票小标 */}
+                            <div className='seletu-biao'>购票</div>
+                            {/* 电影名：name={name} */}
+                            <div className='seletu-tu-name'><a href='#' style={{ color: 'black' }}> ...</a></div>
+                            {/* 评分：score={score} */}
+                            <div className='seletu-tu-score'>暂无评分</div>
+                        </div>
+                    </li><li>
+                        <div className='seletu-name' >
+                            {/* 图片 */}
+                            <div className='seletu-tu'>
+                                <a href='#' >
+                                    <div >
+                                        {/* src={src} */}
+                                        <img
+                                            src="" />
+                                    </div>
+                                </a>
+                            </div>
+                            {/* 购票小标 */}
+                            <div className='seletu-biao'>购票</div>
+                            {/* 电影名：name={name} */}
+                            <div className='seletu-tu-name'><a href='#' style={{ color: 'black' }}> ...</a></div>
+                            {/* 评分：score={score} */}
+                            <div className='seletu-tu-score'>暂无评分</div>
+                        </div>
+                    </li><li>
+                        <div className='seletu-name' >
+                            {/* 图片 */}
+                            <div className='seletu-tu'>
+                                <a href='#' >
+                                    <div >
+                                        {/* src={src} */}
+                                        <img
+                                            src="" />
+                                    </div>
+                                </a>
+                            </div>
+                            {/* 购票小标 */}
+                            <div className='seletu-biao'>购票</div>
+                            {/* 电影名：name={name} */}
+                            <div className='seletu-tu-name'><a href='#' style={{ color: 'black' }}> ...</a></div>
+                            {/* 评分：score={score} */}
+                            <div className='seletu-tu-score'>暂无评分</div>
+                        </div>
+                    </li><li>
+                        <div className='seletu-name' >
+                            {/* 图片 */}
+                            <div className='seletu-tu'>
+                                <a href='#' >
+                                    <div >
+                                        {/* src={src} */}
+                                        <img
+                                            src="" />
+                                    </div>
+                                </a>
+                            </div>
+                            {/* 购票小标 */}
+                            <div className='seletu-biao'>购票</div>
+                            {/* 电影名：name={name} */}
+                            <div className='seletu-tu-name'><a href='#' style={{ color: 'black' }}> ...</a></div>
+                            {/* 评分：score={score} */}
+                            <div className='seletu-tu-score'>暂无评分</div>
+                        </div>
+                    </li>
+                    
+                </ul>
+              </div>
+        <td>{product.name}</td>
+        <td>{product.price}</td>
+      </tr>
+    );
+  }
 
 export default Searchmain
