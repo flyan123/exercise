cb8a12e01452d3ffd81c0b95e643aa0ad3657691
flyan123 <2495365901@qq.com>
2024-02-15 23:55:40 +0800

    add new修改

--- src/components/TopNav/indext.jsx
+++ src/components/TopNav/indext.jsx
@@ -1,52 +1,58 @@
-import React, { useState }  from "react";
+import React, { useState } from "react";
 import "./style.less"
 // 跳转路由
 import { NavLink } from "react-router-dom";
 import { Outlet } from "react-router-dom";
+import { UserOutlined } from '@ant-design/icons';
+import { Avatar, Space } from 'antd';
 
 
-const TopNav=()=>{
-    return(
+const TopNav = () => {
+    return (
         <div className="nav-footer">
             <ul className="clear-fix">
-               {/* <p id="address">定位</p> */}
+                {/* <p id="address">定位</p> */}
                 {/* <Location className="address"/> */}
                 <li>
                     {/* exact高亮 */}
-                    
+
                     <NavLink exact to='/' id="homename" >
                         主页
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink  to='/nowfil' id="shopname">
+                    <NavLink to='/nowfil' id="shopname">
                         分类
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink  to='/user' id="username">
-                        用户
+                    <NavLink to='/user' id="username">
+                        <Space direction="vertical" size={16}>
+                            <Space wrap size={16}>
+                                <Avatar shape="square" size={50} icon={<UserOutlined />} />
+                            </Space>
+                        </Space>
                     </NavLink>
                 </li>
             </ul>
         </div>
     )
 }
-function Location (){
-    const [city,setCity]=useState(
+function Location() {
+    const [city, setCity] = useState(
 
     )
     // 点击定位：左显示省份，点击省份，出现城市
-    return(
+    return (
         <div>
-    <p className="address" onChange>定位</p>
-        <form>
-            <select>
-                <option>12</option>
-                <option>1</option>
-                <option>1</option>
-            </select>
-        </form>
+            <p className="address" onChange>定位</p>
+            <form>
+                <select>
+                    <option>12</option>
+                    <option>1</option>
+                    <option>1</option>
+                </select>
+            </form>
         </div>
 
     )
